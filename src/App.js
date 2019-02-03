3b6672e557d209267ac9864d389dbcb606bd23b4
Melvin Almonte <melvinalmonte@gmail.com>
2019-02-03 09:42:03 -0500

    added container to hold buttons with different messages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {Button} from 'reactstrap'
+import "./Styles/Styles.css"
 
 class App extends Component {
   constructor(props){
@@ -7,28 +8,45 @@ class App extends Component {
     this.state = {
       message:"Hello World."
     };
-    this.hello = this.hello.bind(this);
-    this.goodBye = this.goodBye.bind(this);
+
+    this.message1 = this.message1.bind(this);
+    this.message2 = this.message2.bind(this);
+    this.message3 = this.message3.bind(this);
+    this.message4 = this.message4.bind(this);
   }
 
-  hello(){
+  message1(){
     this.setState({
-      message:"Hello Again!"
+      message:"Message 1"
     })
   }
-  goodBye(){
+  message2(){
     this.setState({
-      message:"Goodbye World!"
+      message:"Message 2"
+    })
+  }
+  message3(){
+    this.setState({
+      message:"Message 3"
+    })
+  }
+  message4(){
+    this.setState({
+      message:"Message 4"
     })
   }
 
 
   render() {
     return (
-        <div>
-          <h1>{this.state.message}</h1>
-          <Button onClick={this.hello}>Hello</Button>
-          <Button onClick={this.goodBye}>Goodbye</Button>
+        <div className="app">
+          <form className="message">{this.state.message}</form>
+          <div className="grid-container">
+            <Button id="my-btn" onClick={this.message1}>Button 1</Button>
+            <Button id="my-btn" onClick={this.message2}>Button 2</Button>
+            <Button id="my-btn" onClick={this.message3}>Button 3</Button>
+            <Button id="my-btn" onClick={this.message4}>Button 4</Button>
+          </div>
         </div>
     );
   }

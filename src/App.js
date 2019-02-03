53b5a0638360a4252858bf9fae7850897855f998
Melvin Almonte <melvinalmonte@gmail.com>
2019-02-03 11:38:09 -0500

    added fetch method which gets title info  from jsonplaceholder website

--- src/App.js
+++ src/App.js
@@ -6,13 +6,43 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      message:"Hello World."
+      message:"Hello World.",
+      title:""
     };
 
     this.message1 = this.message1.bind(this);
     this.message2 = this.message2.bind(this);
     this.message3 = this.message3.bind(this);
     this.message4 = this.message4.bind(this);
+    this.receiveJson = this.receiveJson.bind(this);
+  }
+
+  componentDidMount() {
+    this.receiveJson();
+  }
+
+  receiveJson(){
+    let options = {
+      headers:{
+        "Content-type": "application/json"
+      },
+      method: "GET"
+    };
+
+    fetch("https://jsonplaceholder.typicode.com/posts", options)
+        .then((response) => {
+          return response.json()
+        })
+        .then((data) => {
+          this.setState({
+            title:JSON.stringify(data[0].title)
+          })
+        })
+        .catch(() =>{
+          this.setState({
+            title:"ERROR: Could not fulfill your request."
+          })
+        });
   }
 
   message1(){
@@ -41,6 +71,7 @@ class App extends Component {
     return (
         <div className="app">
           <form className="message">{this.state.message}</form>
+          <form>{this.state.title}</form>
           <div className="grid-container">
             <Button id="my-btn" onClick={this.message1}>Button 1</Button>
             <Button id="my-btn" onClick={this.message2}>Button 2</Button>
